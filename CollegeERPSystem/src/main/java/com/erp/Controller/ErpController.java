084dbb2a89e94712733a66119420941a9d497472
Arjun Mahale <maheshmahale588@gmail.com>
2025-08-19 21:38:01 +0530

    Add error page and update project settings
    Added error.html template for error handling. Updated index.html and style.css, modified ErpController.java, and changed project preferences to reflect new artifact and package names. Increased popularity tracker counts and updated Eclipse workspace settings.

--- CollegeERPSystem/src/main/java/com/erp/Controller/ErpController.java
+++ CollegeERPSystem/src/main/java/com/erp/Controller/ErpController.java
@@ -1,9 +1,12 @@
 package com.erp.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ErpController {
 
 	@GetMapping("/")
@@ -11,4 +14,22 @@ public class ErpController {
 	{
 		return "index";
 	}
+	
+	  @PostMapping("/login")
+	    public String login(
+	            @RequestParam("role") String role,
+	            @RequestParam("username") String username,
+	            @RequestParam("password") String password) {
+
+	        // Example check
+	        if ("admin".equals(role) && "admin".equals(username) && "1234".equals(password)) {
+	            return "redirect:/admin-dashboard";
+	        } else if ("faculty".equals(role)) {
+	            return "redirect:/faculty-dashboard";
+	        } else if ("student".equals(role)) {
+	            return "redirect:/student-dashboard";
+	        } else {
+	            return "redirect:/login?error"; // invalid login
+	        }
+	    }
 }
